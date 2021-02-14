efae47675825adf830e47a972e4f081f20d4c39a
ArfOz <arfoz@gmail.com>
2021-02-14 18:04:34 +0300

    updated

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,17 @@
 import React, {useState, useEffect} from 'react';
 import {SafeAreaView, Text, View, Image, TextInput, Button, Alert} from "react-native";
-import axios from "axios";
+import {API_KEY} from "@env";
 
 const Main = (props) => {
-    const [lat, setLat] = useState([]);
-    const [lon, setLon] = useState([]);
+    const [lat, setLat] = useState("1.5");
+    const [lon, setLon] = useState("100.75");
+    console.log(API_KEY)
 
-const sendingValue=()=>{ props.navigation.navigate("Select", {lat, lon})};
+    var base_url2 = `https://api.nasa.gov/planetary/earth/imagery?lon=${lon}&lat=${lat}&date=2014-02-01&api_key=${API_KEY}`
+    console.log(base_url2)
+
+  
+const sendingValue=(lon,lat)=>{ props.navigation.navigate("Select", { base_url : base_url2})};
 
     return (
         <SafeAreaView>
@@ -14,6 +19,7 @@ const sendingValue=()=>{ props.navigation.navigate("Select", {lat, lon})};
            source={require("../assets/logo.jpg")}
            />
            <TextInput
+           keyboardType={'numeric'}
             style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
             onChangeText={text => setLat(text)}
             value={lat}
@@ -21,6 +27,7 @@ const sendingValue=()=>{ props.navigation.navigate("Select", {lat, lon})};
         />
         
         <TextInput
+            keyboardType={'numeric'}
             style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
             onChangeText={text => setLon(text)}
             value={lon}
--- src/pages/Select.js
+++ src/pages/Select.js
@@ -1,37 +1,33 @@
 import React, {useEffect, useState} from 'react';
 import {SafeAreaView, Text, View, Image, Button} from "react-native";
 import axios from "axios";
+import {Buffer} from "buffer";
+import base64 from 'react-native-base64'
 
 const Select = (props) => {
+    const [data, setData] = useState([])
 
-    const lat = props.route.params.lat;
-    const lon = props.route.params.lon;
-    console.log(lat);
-    const key = "HwBkfWKy5nOJMvCg10J55Zfkok92ejcUa0ZhBgsA";
-    console.log(lon);
+    const base_url = props.route.params.base_url;
+    console.log(base_url)
     console.log("ariffffffffffffffffffff")
 
     const fetchData = async(lat, lon, key) =>{
-        const response = await axios.get("https://api.nasa.gov/planetary/earth/imagery",{params:{
-            lon : 100.75,
-            lat : 1.5,
-            date : "2014-02-01",
-            api_key: "HwBkfWKy5nOJMvCg10J55Zfkok92ejcUa0ZhBgsA"
-        }});
+        const response = await axios.get(base_url,{responseType:"arraybuffer"}).then(response => Buffer.from(response.data, 'binary').toString('base64'));
         console.log("response", response);
-        console.log("veri gitmiş olmalı")
-        
-    }
+        setData(response);
+       
+    };
+    var base64Icon = `data:image/png;base64,${data}`;
+    console.log(base64Icon) 
+
     useEffect(()=>{
         fetchData();
     }, [])
     const goBack = () =>{ props.navigation.navigate("Main")}
     return (
-        <SafeAreaView>
-            <Text>
-                {lat}
-                {lon}
-            </Text>
+        <SafeAreaView style={{flex:1}}>
+            <Image style={{flex:1,  borderWidth: 1, borderColor: 'red'}} source={{uri: base64Icon}}/>
+
             <Button
         title="New Search"
         onPress={goBack}
